85acf73eb3d477579beef80a6cde9051c3296fd3
KateDro <106846150+KateDro@users.noreply.github.com>
2023-11-21 12:01:41 +0100

    dz2 complete

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('form.feedback-form'),
+  input: document.querySelector("input[name='email']"),
+  textarea: document.querySelector("textarea[name='message']"),
+};
+
+const formInputValues = {
+  email: '',
+  message: '',
+};
