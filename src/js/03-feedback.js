fa3aca8babe309bea1f8d0f38e033dc8dcfe5d55
KateDro <106846150+KateDro@users.noreply.github.com>
2023-11-21 13:48:39 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,3 +10,58 @@ const formInputValues = {
   email: '',
   message: '',
 };
+
+setValueToInput();
+
+refs.form.addEventListener('input', throttle(onInputChangesSaveValue, 500));
+refs.form.addEventListener('submit', onSubmit);
+
+function onInputChangesSaveValue(event) {
+  // console.log(formInputValues);
+
+  if (event.target.nodeName === refs.input.nodeName) {
+    formInputValues.email = event.target.value;
+  } else {
+    formInputValues.message = event.target.value;
+  }
+
+  // console.log(formInputValues);
+  saveValueToLocalStorage();
+  // console.log(saveValueToLocalStorage(formInputValues));
+}
+
+function saveValueToLocalStorage() {
+  localStorage.setItem('feedback-form-state', JSON.stringify(formInputValues));
+
+  // return localStorage.getItem("feedback-form-state");
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  console.log(formInputValues);
+
+  event.currentTarget.reset();
+  localStorage.clear();
+
+  // console.log(formInputValues);
+  // console.log(localStorage);
+}
+
+function setValueToInput() {
+  if (localStorage.getItem('feedback-form-state') === null) {
+    // console.log('localStorage порожній');
+    return;
+  }
+
+  const tempValues = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  refs.input.value = tempValues.email;
+  formInputValues.email = tempValues.email;
+  refs.textarea.value = tempValues.message;
+  formInputValues.message = tempValues.message;
+
+  // console.log('пушимо дані з localStorage');
+  // console.log(tempValues);
+  // console.log(formInputValues);
+}
